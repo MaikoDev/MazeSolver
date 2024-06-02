66004ee4f2238fabe0807003835f199f6eafd22d
Phyo Naung <phyonaung32@yahoo.com>
2024-06-01 21:08:52 -0700

    Added contiguous arrays to NavigationMap

--- src/main/java/org/maikodev/app/nav/NavigationMap.java
+++ src/main/java/org/maikodev/app/nav/NavigationMap.java
@@ -1,15 +1,40 @@
-package org.maikodev.nav;
+package org.maikodev.app.nav;
 
-import org.maikodev.interfaces.IObservable;
+import org.maikodev.app.interfaces.IObservable;
+import org.maikodev.app.enums.NodeState;
 
 import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Objects;
 
-public final class NavigationMap implements IObservable {
-    public NavigationMap(int rowCount, int columnCount) {
+public class NavigationMap implements IObservable {
+    public NavigationMap(short rowCount, short columnCount) {
         this.rowCount = rowCount;
         this.columnCount = columnCount;
 
-        navTable = new NavigationNode[rowCount * columnCount];
+        nodeCount = (int)rowCount * columnCount;
+
+        nodeStates = new NodeState[nodeCount];
+        nodeHashes = new int[nodeCount];
+
+        gCostTable = new int[nodeCount];
+        hCostTable = new int[nodeCount];
+        fCostTable = new int[nodeCount];
+
+        redirectionTable = new NavPoint[nodeCount];
+        neighborsMaxHeap = new NodeCostPair[nodeCount];
+
+        hashToHeapIndexMap = new HashMap<>();
+
+        // Setup Redirection table //
+        for (short row = 0; row < rowCount; row++) {
+            for (short column = 0; column < columnCount; column++) {
+                int currentIndex = getIndex(row, column, columnCount);
+
+                nodeHashes[currentIndex] = Objects.hash(row, column);
+                redirectionTable[currentIndex] = new NavPoint(row, column);
+            }
+        }
     }
 
     @Override
@@ -24,12 +49,29 @@ public final class NavigationMap implements IObservable {
 
     @Override
     public ActionListener[] getActionListeners() {
-
+        return null;
     }
 
-    private int rowCount;
-    private int columnCount;
-    private NavigationNode[] navTable;
+    protected static int getIndex(short row, short column, short maxColumns) {
+        return row * maxColumns + column;
+    }
 
+    protected short rowCount;
+    protected short columnCount;
+    protected int nodeCount;
+    protected int neighborsCount;
 
+    /* Contiguous arrays for node state */
+    protected NodeState[] nodeStates;
+    protected int[] nodeHashes;
+    protected int[] gCostTable;
+    protected int[] hCostTable;
+    protected int[] fCostTable;
+    protected NavPoint[] redirectionTable;
+
+    /* Binary Heap for lowest fCost Neighbor */
+    protected NodeCostPair[] neighborsMaxHeap;
+
+    /* Look-up */
+    protected HashMap<Integer, Integer> hashToHeapIndexMap;
 }
